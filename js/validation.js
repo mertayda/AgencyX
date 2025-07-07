5a634cc47fd96bb1ead9795cf2bf3f42aea07dcd
Mert Ayda <mrtayda@gmail.com>
2025-07-07 05:12:06 +0300

    footer finished

--- js/validation.js
+++ js/validation.js
@@ -1,42 +1,61 @@
-
+const form = document.querySelector(".nm-form")
 const newsletter = document.querySelector(".newsletter")
 const emailInput = document.querySelector(".emailInput")
 const submitButton = document.querySelector("#form-btn")
 const warning = document.querySelector(".warning")
 const successWarning = document.querySelector(".success-warning")
-const successEmail = document.querySelector(".succesful-email")
+const closeBtn = document.querySelector(".close-btn")
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-
-
-
-function showSuccess(){
-     successWarning.classList.add("show")
-}
-
-
-function emailValid(e){
-       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-       if(!emailRegex.test(emailInput.value)){
+form.addEventListener("submit", async (e) => {
+      e.preventDefault()
+      let email = emailInput.value
+      if (!emailRegex.test(email)) {
          warning.classList.add("info")
-      } else {
-         warning.classList.remove("info")
-       }
-    
+         setTimeout(() => {
+            warning.classList.remove("info")
 
-}
+         }, 4000)
+         return;
+      }
 
 
-submitButton.addEventListener("click", (e) => {
-     e.preventDefault()
-     showSuccess()
+      try {
+         submitButton.disabled = true;
+         warning.classList.add("info")
+         warning.innerHTML = "Sending"
+         const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+               email: email
+            }),
+         })
 
+         if (!response.ok) {
+            throw new Error(`hata ${response}`)
+         }
+
+
+         const data = await response.json()
+         console.log(data)
+         successWarning.classList.add("show")
+
+      } catch (error) {
+         console.error(error)
+         warning.classList.add("info")
+         warning.innerHTML = "Error Happened"
+      }
+
+   }
+
+)
+
+closeBtn.addEventListener("click", () => {
+   successWarning.classList.remove("show")
+   warning.innerHTML = ""
 })
-
-
-emailInput.addEventListener("input", emailValid)
-
-
-
-
-
